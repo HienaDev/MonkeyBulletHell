1212e78edf516cb2402910b04c682e74598d2cc7
HienaDev <ant.rodrigues1999@gmail.com>
2024-11-13 12:05:44 +0000

    Add island stuff

--- Assets/SpawnMoaiOnCollision.cs
+++ Assets/SpawnMoaiOnCollision.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class SpawnMoaiOnCollision : MonoBehaviour
 {
     [SerializeField] private GameObject moai;
+    [SerializeField] private GameObject rockDoor;
 
     private void OnCollisionEnter(Collision collision)
     {
         moai.SetActive(true);
+        rockDoor.SetActive(true);
     }
 }
