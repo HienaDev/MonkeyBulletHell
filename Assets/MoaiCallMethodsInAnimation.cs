b85655eec298a5320c87edbea00de35aec5063a1
HienaDev <ant.rodrigues1999@gmail.com>
2025-01-12 15:55:53 +0000

    Add camera shake to attacks

--- Assets/MoaiCallMethodsInAnimation.cs
+++ Assets/MoaiCallMethodsInAnimation.cs
@@ -4,20 +4,25 @@ public class MoaiCallMethodsInAnimation : MonoBehaviour
 {
     [SerializeField] private AttackPatterns attackPattern;
     [SerializeField] private Collider damageCollider;
+    [SerializeField] private TriggerCameraShake cameraShake;
 
     public void HandStomp()
     {
+        cameraShake.TriggerShake();
         attackPattern.HandStompCalledInAnimation();
     }
 
     public void FastHandStomp()
     {
+        cameraShake.TriggerShake();
         attackPattern.FastStomp();
     }
 
     public void FlyAndStomp()
     {
+        cameraShake.TriggerShake();
         attackPattern.FlyAndStomp();
+
     }
 
     public void BecomeImmortal() => damageCollider.enabled = (false);
